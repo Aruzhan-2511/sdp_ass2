8c6c7abc8704efab17b635c12b70c1193da02873
Aruzhan-2511 <aruzhan.21012008@icloud.com>
2026-09-20 01:27:12 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
+        System.out.println("---Factory Method---");
         IDanceClassFactory balletFactory = new BalletClassFactory();
         IDanceClass ballet = balletFactory.createDanceClass();
         ballet.startClass();;
@@ -7,5 +8,16 @@ public class Main {
         IDanceClassFactory hipHopFactory = new HipHopClassFactory();
         IDanceClass hipHop = hipHopFactory.createDanceClass();
         hipHop.startClass();
+        System.out.println("---Abstract Factory---");
+        IDanceStudioFactory balletStudio = new BalletStudioFactory();
+        runDanceStudio(balletStudio);
+        IDanceStudioFactory hipHopStudio = new HipHopStudioFactory();
+        runDanceStudio(hipHopStudio);
+    }
+    private static void runDanceStudio(IDanceStudioFactory factory){
+        IDanceClass danceClass = factory.createDanceClass();
+        IDanceOutfit danceOutfit = factory.createDanceOutfit();
+        danceClass.startClass();;
+        danceOutfit.wearOutfit();
     }
 }
